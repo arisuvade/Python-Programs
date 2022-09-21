1e7a171393d063d163fae43212338b5296106ca7
Aries Dave Bautista <110705264+arisuvade@users.noreply.github.com>
2022-09-21 22:24:58 +0800

    Add files via upload

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,19 +1,15 @@
 def main():
     text = get_text()
-    result = palindrome_validator(text)
+    result = is_palindrome(text)
     print(result)
 
 
 def get_text():
-    while True:
-        text = input("Text: ")
-        if text.isalpha():
-            return text
-        else:
-            continue
+    text = input("Text: ")
+    return text
 
 
-def palindrome_validator(text):
+def is_palindrome(text):
     if text == text[::-1]:
         return f"{text.capitalize()} is a palindrome."
     else:
